64b5db7ce87b638b3638b9195a6831db4aac74d2
Uzer Jamal <uzerjamal@gmail.com>
2019-09-18 07:29:27 +0530

    Test

--- pageGenerator.py
+++ pageGenerator.py
@@ -18,6 +18,7 @@ titleCloseHtml = '''
 '''
 
 topHtml = '''
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
 <link rel="stylesheet" href="css/style.css">
         <script src="js/jquery.js"></script> 
         <script> 
